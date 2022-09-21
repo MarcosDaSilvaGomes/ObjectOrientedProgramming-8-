7590a220f61819a2d774de13d501a23b12bfde97
MarcosDaSilvaGomes <marcossgoficial@hotmail.com.br>
2022-09-20 23:51:29 -0300

    Java8

---
+++ src/script.ts
@@ -0,0 +1,20 @@
+class cliente {
+    
+    public name: string;
+    public email: String;
+    public idade : number;
+    public street: String;
+
+        }
+   { const cliente:Clientes = new Clientes ();
+    const nome  : 'Leo';
+    const email : 'Leo21@gmail.com';
+    const idade : '19';
+    const street: 'Rio de janeiro'
+
+console.log(`-Novo Cliente-`);
+console.log(`Nome do cliente é ${cliente.nome}`);
+console.log(`E-mail de cadastro é ${cliente.email}`);
+console.log(`Idade do cliente ${cliente.idade}`);
+console.log(`Mora no :  ${street}´)
+   }
